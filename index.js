6a8bdf5371b34b0d220e0545797f371c75b45b82
GavinH02 <122926635+GavinH02@users.noreply.github.com>
2023-04-12 20:09:06 -0500

    added part of the button functionallity

--- index.js
+++ index.js
@@ -3,8 +3,70 @@ import ReactDOM from "react-dom/client";
 import Shop from "./shop/Shop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode><Shop /></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <Shop />
+  </React.StrictMode>
+);
 
-fetch('https://fakestoreapi.com/products/1')
-            .then(res=>res.json())
-            .then(json=>console.log(json));
+fetch("https://fakestoreapi.com/products/1")
+  .then((res) => res.json())
+  .then((json) => console.log(json));
+
+
+let cart = {};
+
+cart = {
+  p1num: 0,
+  p2num: 0,
+  p3num: 0,
+  p4num: 0,
+  p5num: 0,
+  p6num: 0,
+};
+
+function increment(a) {
+  switch (a) {
+    case "p1num":
+      cart.p1num++;
+      break;
+    case "p2num":
+      cart.p2num++;
+      break;
+    case "p3num":
+      cart.p3num++;
+      break;
+    case "p4num":
+      cart.p4num++;
+      break;
+    case "p5num":
+      cart.p5num++;
+      break;
+    case "p6num":
+      cart.p6num++;
+      break;
+  }
+}
+
+function decrement(a) {
+  switch (a) {
+    case "p1num":
+      cart.p1num--;
+      break;
+    case "p2num":
+      cart.p2num--;
+      break;
+    case "p3num":
+      cart.p3num--;
+      break;
+    case "p4num":
+      cart.p4num--;
+      break;
+    case "p5num":
+      cart.p5num--;
+      break;
+    case "p6num":
+      cart.p6num--;
+      break;
+  }
+}
