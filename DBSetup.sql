465fc04570abaa1ded9d728d5713b63c81817bcd
diamondrules1 <ajf61302@gmail.com>
2023-05-06 23:56:41 -0500

    removed allprintings

--- DBSetup.sql
+++ DBSetup.sql
@@ -2,4 +2,6 @@ drop database if exists `reactmysql`;
 create database `reactmysql`;
 use `reactmysql`;
 drop table if exists User;
-create table User(id int unique, username varchar(50), password varchar(50), email varchar(50), primary key(id));
+drop table if exists Card;
+create table User(uid int unique, username varchar(50), password varchar(50), email varchar(50), primary key(uid));
+create table Card(cid int unique, cardname varchar(50), price double, rarity varchar(20), cardSet varchar(50), description varchar(200), quantity int, primary key (cid));
