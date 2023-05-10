67d685e7afc6623108988ee57c93e8e356648ab9
diamondrules1 <ajf61302@gmail.com>
2023-05-10 00:34:10 -0500

    deleted sql stuff

--- DBSetup.sql
+++
@@ -1,5 +0,0 @@
-drop database if exists `reactmysql`;
-create database `reactmysql`;
-use `reactmysql`;
-drop table if exists Card;
-create table Card(cid int unique, cardname varchar(50), price double, rarity varchar(20), cardSet varchar(50), description varchar(200), quantity int, primary key (cid));
