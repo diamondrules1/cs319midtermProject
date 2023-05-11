dd7cc481ad85fd56eec20b4248a58a7e1f56a967
GavinH02 <122926635+GavinH02@users.noreply.github.com>
2023-05-10 22:03:22 -0500

    Reformated App.js

--- rest_with_mysql_frontend/src/App.js
+++ rest_with_mysql_frontend/src/App.js
@@ -1,99 +1,107 @@
-import './App.css';
-import {useState, useEffect} from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
   const [card, setCard] = useState([]);
   const [viewer1, setViewer1] = useState(false);
   const [oneCard, setOneCard] = useState([]);
   const [viewer2, setViewer2] = useState(false);
-  const [query, setQuery] = useState('');
-  
+  const [query, setQuery] = useState("");
+
   const handleChange = (e) => {
-  setQuery(e.target.value);
-  const results = card.filter(oneCard => {
-    if (e.target.value === "") return card;
-    return oneCard.name.toLowerCase().includes(e.target.value.toLowerCase())
-  });
-  setCard(results);
-  }
+    setQuery(e.target.value);
+    const results = card.filter((oneCard) => {
+      if (e.target.value === "") return card;
+      return oneCard.name.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    setCard(results);
+  };
 
   const showCardResult = oneCard.map((el) => {
-  if(el.quantity > 0)
-  return (
-  <div key={el.cid}>
-  Name: {el.cardName} <br />
-  Price: {el.price} <br />
-  Rarity: {el.rarity} <br />
-  Color: {el.color} <br />
-  Card Set: {el.cardSet} <br />
-  Description: {el.description} <br />
-  In Stock: {el.quantity} <br />
-  <button onClick={reduceStock(el)}>Set One Aside for Me?</button>
-  </div>
-); else return;});
-
-const showCardResults = card.map((el) => {
-  <div key={el.cid}>
-  Name: {el.cardName} <br />
-  Price: {el.price} <br />
-  Rarity: {el.rarity} <br />
-  Color: {el.color} <br />
-  Card Set: {el.cardSet} <br />
-  Description: {el.description} <br />
-  In Stock: {el.quantity} <br />
-  </div>
-});
-
-function getAllCards(){
-  fetch("http://127.0.0.1:4000/getAllCards/")
-  .then((response) => response.json())
-  .then((data) => {
-      console.log("Show all cards: ",data);
-      const dataArr = [];
-      dataArr.push(data);
-      setCard(data);
+    if (el.quantity > 0)
+      return (
+        <div key={el.cid}>
+          Name: {el.cardName} <br />
+          Price: {el.price} <br />
+          Rarity: {el.rarity} <br />
+          Color: {el.color} <br />
+          Card Set: {el.cardSet} <br />
+          Description: {el.description} <br />
+          In Stock: {el.quantity} <br />
+          <button onClick={reduceStock(el)}>Set One Aside for Me?</button>
+        </div>
+      );
+    else return;
   });
-  setViewer1(!viewer1);
-}
 
-function getCardByName(cardName){
-  console.log(cardName);
-      fetch("http://127.0.0.1:4000/getCardByName/"+cardName)
+  const showCardResults = card.map((el) => {
+    <div key={el.cid}>
+      Name: {el.cardName} <br />
+      Price: {el.price} <br />
+      Rarity: {el.rarity} <br />
+      Color: {el.color} <br />
+      Card Set: {el.cardSet} <br />
+      Description: {el.description} <br />
+      In Stock: {el.quantity} <br />
+    </div>;
+  });
+
+  function getAllCards() {
+    fetch("http://127.0.0.1:4000/getAllCards/")
       .then((response) => response.json())
       .then((data) => {
-          console.log("Show one card: ", cardName);
-          console.log(data);
-          const dataArr = [];
-          dataArr.push(data);
-          setOneCard(data);
+        console.log("Show all cards: ", data);
+        const dataArr = [];
+        dataArr.push(data);
+        setCard(data);
       });
-      setViewer2(!viewer2);
-}
+    setViewer1(!viewer1);
+  }
 
-function reduceStock(el){
-  let name = prompt("Please enter your name: ");
-  if (name !== ""){
-    console.log(el.cid, el.quantity);
-      fetch("http://127.0.0.1:4000/update/"+el.cid+"/"+el.quantity)
+  function getCardByName(cardName) {
+    console.log(cardName);
+    fetch("http://127.0.0.1:4000/getCardByName/" + cardName)
       .then((response) => response.json())
       .then((data) => {
+        console.log("Show one card: ", cardName);
+        console.log(data);
+        const dataArr = [];
+        dataArr.push(data);
+        setOneCard(data);
+      });
+    setViewer2(!viewer2);
+  }
+
+  function reduceStock(el) {
+    let name = prompt("Please enter your name: ");
+    if (name !== "") {
+      console.log(el.cid, el.quantity);
+      fetch("http://127.0.0.1:4000/update/" + el.cid + "/" + el.quantity)
+        .then((response) => response.json())
+        .then((data) => {
           console.log("Show one card: ", el.cid);
           console.log(data);
           const dataArr = [];
           dataArr.push(data);
           setOneCard(data);
-      });
+        });
       setViewer2(!viewer2);
+    }
   }
-}
 
-return (
+  return (
     <div>
       <h1>Cards</h1>
       <button onClick={() => getAllCards()}>Show All Cards</button>
       <label for="cardName">Search by Card Name:</label>
-      <input type="text" id="cardName" name="cardName" value={query} onChange={handleChange}/>
-      <hr></hr>  
+      <input
+        type="text"
+        id="cardName"
+        name="cardName"
+        value={query}
+        onChange={handleChange}
+      />
+      <hr></hr>
       {viewer1 && <div>Cards {showCardResults}</div>}
       <hr></hr>
       <h1>Searched Card</h1>
@@ -102,12 +110,5 @@ return (
     </div>
   );
 }
-
-
-
-
-
-
-
 
 export default App;
